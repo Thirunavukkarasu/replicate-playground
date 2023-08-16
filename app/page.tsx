eefcd9504e34af2940f057a15fdd19c847e2b368
thirunavukkarasu <thirunavukkarasu.sam@gmail.com>
2023-08-16 20:13:02 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { MainNav } from "@/components/main-nav";
 import Form from "./form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Form />
-    </main>
+    <>
+      <MainNav />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Form />
+      </main>
+    </>
   );
 }
